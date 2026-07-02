c1e1ad9c3de01b76404bc4ac919a78fcbbb7609d
divyanshmakkar17 <divyanshmakkar17@gmail.com>
2026-07-03 02:37:14 +0530

    Completed Day 2 control flow and loops

---
+++ python/multiplication_table.py
@@ -0,0 +1,7 @@
+number = float(input("Enter your number : "))
+
+for i in range(0,11) :
+    print(number , "x" , i , "=" , number * i)
+mark = int(input("Enter your number : "))
+for i in range(mark,100) :
+    print(i)
---
+++ python/table_of2.py
@@ -0,0 +1,22 @@
+a = 2
+b = 4 
+c = 6
+d = 8
+e = 10
+f = 12
+g = 14
+h = 16 
+i = 18
+j = 20
+
+print("Table of 2:")
+print("2*1= " , a)
+print("2*2= " , b)
+print("2*3= " , c)
+print("2*4= " , d)
+print("2*5= " , e)
+print("2*6= " , f)
+print("2*7= " , g)
+print("2*8= " , h)
+print("2*9= " , i)
+print("2*10= " , j)
